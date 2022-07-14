422b826bc607328dfc7f5ecdaa2a992a92fb4cb5
Jasper Haggenburg <jasper@pressplay.dev>
2022-07-14 15:29:32 +0200

    Upgrade samen

--- web/client/src/index.tsx
+++ web/client/src/index.tsx
@@ -3,8 +3,5 @@ import { SamenClient } from "@samen/client"
 const fetch = window.fetch.bind(window)
 const client = new SamenClient(fetch, process.env.REACT_APP_SAMEN_URL)
 
-async function run() {
-  console.log(await client.articleService.get("1"))
-}
-
-export default run()
+const w = window as any
+w.client = client
