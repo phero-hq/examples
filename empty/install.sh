52c8d669e6f67cbfe93714a4cef674504995d274
Jasper Haggenburg <jasper@pressplay.dev>
2023-01-17 15:27:47 +0100

    remove empty project

--- empty/install.sh
+++
@@ -1,6 +0,0 @@
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-cd $SCRIPT_DIR
-
-(cd ../../phero && ./node_modules/.bin/lerna bootstrap)
-
-npm uninstall -g phero && npm install -g ../../phero/packages/cli
--- web/install.sh
+++
@@ -1,9 +0,0 @@
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-cd $SCRIPT_DIR
-
-(cd ../../phero && ./node_modules/.bin/lerna bootstrap)
-
-npm uninstall -g phero && npm install -g ../../phero/packages/cli
-
-(cd ./client && npm uninstall @phero/client && npm install ../../../phero/packages/client)
-(cd ./server && npm uninstall @phero/server && npm install ../../../phero/packages/server)
