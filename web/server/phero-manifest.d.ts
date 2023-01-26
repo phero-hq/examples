9d8d9556738601069d9ea45fa27ec9ecf4ed619a
Jasper Haggenburg <jasper@pressplay.dev>
2023-01-26 20:43:42 +0100

    Regenerate manifest

--- web/server/phero-manifest.d.ts
+++ web/server/phero-manifest.d.ts
@@ -1,13 +1,28 @@
 export declare namespace domain {
     namespace v_1_0_0 {
+        interface User {
+            uid: string;
+            name: string;
+        }
         interface Article {
             id: string;
             text: string;
         }
     }
 }
+export declare namespace phero {
+    type PheroContext<T> = T;
+}
 export declare namespace articleService {
     namespace v_1_0_0 {
-        function getArticle(id: string): Promise<domain.v_1_0_0.Article>;
+        function getArticle(context: phero.PheroContext<{
+            idToken: string;
+        }>, id: string): Promise<domain.v_1_0_0.Article>;
+        function createArticle(context: phero.PheroContext<{
+            idToken: string;
+        }>, id: string, text: string): Promise<domain.v_1_0_0.Article>;
+        function saveArticle(context: phero.PheroContext<{
+            idToken: string;
+        }>, id: string, text: string): Promise<domain.v_1_0_0.Article>;
     }
 }
